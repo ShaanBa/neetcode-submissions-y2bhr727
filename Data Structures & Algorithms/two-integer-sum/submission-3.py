84d0d37c0d6f754d1d2497a2a2699da8e4786120
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-08-21 10:42:58 -0500

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i in range(len(nums)):
+            hash[nums[i]] = i
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in hash.keys():
+                if hash[complement] != i:
+                    return [i, hash[complement]]
