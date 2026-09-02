f8cc0e5b1c32a60f63d99ddc72aed15846f67aea
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-09-02 15:06:36 -0500

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # if a number appears more than once return true
+        # the approach for this is pretty simple we can use a set it gets rid of duplicates for us and if the len (set(nums)) == len(num) we now its not a dupe
+        # return len(set(nums)) != len(nums)
+        hashmap = {}
+        for i in range(len(nums)):
+            if nums[i] in hashmap:
+                return True
+            else:
+                hashmap[nums[i]] = i
+        return False
